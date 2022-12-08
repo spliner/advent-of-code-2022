52e25fea78fd0502d8fc35c8cb1ff0785f62ed83
Fillipe Rosini <hi@frosini.dev>
2022-12-08 13:39:58 -0300

    Finished day 6 part 2 in Go

--- go/pkg/day6/day6.go
+++ go/pkg/day6/day6.go
@@ -1,7 +1,27 @@
 package day6
 
+import (
+	"aoc2022/pkg/util/set"
+	"strconv"
+)
+
 func Part1(input string) (string, error) {
-	return "", nil
+	marker := marker(input, 4)
+	result := strconv.Itoa(marker)
+	return result, nil
+}
+
+func marker(input string, size int) int {
+	runes := []rune(input)
+	for i := range runes {
+		slice := runes[i : i+size]
+		set := set.NewSetFromSlice(slice)
+		if set.Length() == len(slice) {
+			return i + size
+		}
+	}
+
+	return 0
 }
 
 func Part2(input string) (string, error) {
